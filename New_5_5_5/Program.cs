f2599d8e145af817ef109cad7bbf0388856b2347
Andentu <severalmaz97@gmail.com>
2023-03-03 03:55:53 +0300

    Код рассчета факториала числа (до 27 включительно)

--- New_5_5_5/Program.cs
+++ New_5_5_5/Program.cs
@@ -4,7 +4,24 @@
     {
         static void Main(string[] args)
         {
-            
+            Console.WriteLine("Введите число для рассчета факториала (до 27 включительно): ");
+            var num = decimal.Parse(Console.ReadLine());
+            var num2 = num;
+            Factorial(num);
+            Console.WriteLine("Факториал числа {0}: {1}", num2,Factorial(num));
+            Console.ReadKey();
         }
+        static decimal Factorial(decimal x)
+        {
+            if (x == 0)
+            {
+                return 1;
+            }
+            else
+            {
+                return x * Factorial(x - 1);
+            }
+        }
+        
     }
 }
